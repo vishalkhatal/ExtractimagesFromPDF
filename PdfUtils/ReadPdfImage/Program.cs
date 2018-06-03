606638d45a3b25cc7413502790e9265908ecf003
Vishal Khatal <vishal.khatal2206@gmail.com>
2018-06-03 12:04:24 +0530

    Added src code

---
+++ PdfUtils/ReadPdfImage/Program.cs
@@ -0,0 +1,34 @@
+﻿using PdfUtils;
+using System;
+using System.IO;
+
+namespace ReadPdfImage
+{
+    class Program
+    {
+        static void Main()
+        {
+            var args = Environment.GetCommandLineArgs();
+            var searchPath = Path.Combine(Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(args[0]))), "Data");
+
+            foreach (var filename in Directory.GetFiles(searchPath, "*.pdf", SearchOption.TopDirectoryOnly))
+            {
+                Console.WriteLine("Extracting images from {0}", Path.GetFileName(filename));
+
+                var images = PdfImageExtractor.ExtractImages(filename);
+
+                Console.WriteLine("{0} images found.", images.Count);
+                Console.WriteLine();
+
+                var directory = Path.GetDirectoryName(filename);
+
+                foreach (var name in images.Keys)
+                {
+                    images[name].Save(Path.Combine(directory, name));
+                }
+            }
+            Console.WriteLine("Done.");
+            Console.ReadLine();
+        }
+    }
+}
